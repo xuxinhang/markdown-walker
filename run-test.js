f9160cc7f32bb98f10dcf4bf40f843426f38ef75
Xu Xinhang <xuxinhang4567#126.com>
2019-07-19 21:39:40 +0800

    test: 在测试程序中增加对多段落的支持

--- run-test.js
+++ run-test.js
@@ -9,20 +9,23 @@ var parse = require('./src/parser.ts').default;
 var render = require('./src/render.ts').default;
 
 var content = fs.readFileSync('./__testcase/spec/emphasis_and_strong_emphasis.json', { encoding: 'utf-8' });
+// var content = fs.readFileSync('./__testcase/spec/links.json', { encoding: 'utf-8' });
 var testcases = JSON.parse(content);
 
-testcases.forEach(cas => {
+for (const cas of testcases) {
+  if (cas.number >= 522) continue;
+
   var source = trimInlineInput(cas.markdown);
-  var ast = parse(source);
-  var html = render(ast);
+  var paraSrc = source.split('\n\n');
+  var html = paraSrc.map(src => render(parse(src))).join('');
   if (html !== cas.html) {
-    console.log(`[TESTCASE #${cas.number}]`);
+    console.log(`[#${cas.number}]`);
     console.log(source);
     console.log('\x1b[32m' + cas.html + '\x1b[0m');
     console.log('\x1b[35m' + html     + '\x1b[0m');
-    console.log('\x1b[37m\n');
+    console.log('\x1b[37m');
   }
-});
+}
 
 
 function trimInlineInput(src) {
